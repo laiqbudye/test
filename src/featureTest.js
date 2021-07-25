c1a75eb45cb78b178be5d89589940b8c52630fda
laiqb <laiqbudye@gmail.com>
2021-07-25 15:10:59 +0530

    this is test commit on feature branch from CLI

---
+++ src/featureTest.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const featureTest = () => {
+    return (
+        <div>
+            <h1>this is test file for feature branch</h1>
+        </div>
+    )
+}
+
+export default featureTest
